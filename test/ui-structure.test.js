e3afa11196296f3011f69a5c5fa823310718fe27
feechkablum6 <mazn.2007@mail.ru>
2026-07-22 12:30:51 +0700

    ui: убрать значки перед устройствами

--- test/ui-structure.test.js
+++ test/ui-structure.test.js
@@ -11,3 +11,12 @@ test('does not render static equalizer preset pills', () => {
     assert.doesNotMatch(html, /preset-pill/);
     assert.doesNotMatch(css, /\.preset-pill/);
 });
+
+test('shows device names without leading link icons', () => {
+    const projectRoot = path.join(__dirname, '..');
+    const html = fs.readFileSync(path.join(projectRoot, 'index.html'), 'utf8');
+    const css = fs.readFileSync(path.join(projectRoot, 'styles.css'), 'utf8');
+
+    assert.doesNotMatch(html, /small-icon/);
+    assert.doesNotMatch(css, /\.device-info \.small-icon/);
+});
