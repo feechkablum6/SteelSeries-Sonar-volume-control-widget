8f619d0e0e3a3e35b91fd4e44d7212a42426fb56
feechkablum6 <mazn.2007@mail.ru>
2026-07-22 12:26:50 +0700

    ui: убрать статические эквалайзеры

---
+++ test/ui-structure.test.js
@@ -0,0 +1,13 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+
+test('does not render static equalizer preset pills', () => {
+    const projectRoot = path.join(__dirname, '..');
+    const html = fs.readFileSync(path.join(projectRoot, 'index.html'), 'utf8');
+    const css = fs.readFileSync(path.join(projectRoot, 'styles.css'), 'utf8');
+
+    assert.doesNotMatch(html, /preset-pill/);
+    assert.doesNotMatch(css, /\.preset-pill/);
+});
